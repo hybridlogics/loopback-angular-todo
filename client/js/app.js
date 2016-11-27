210af1b976257f765d3f23a2c9deb5e118bb1a63
HLNAJAM <najam@hybridlogics.com>
2016-11-27 13:11:27 +0500

    master

---
+++ client/js/app.js
@@ -0,0 +1,21 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app', [
+    'lbServices',
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
+      $urlRouterProvider) {
+    $stateProvider
+      .state('todo', {
+        url: '',
+        templateUrl: 'views/todo.html',
+        controller: 'TodoController'
+      });
+
+    $urlRouterProvider.otherwise('todo');
+  }]);
